6722d54a7609f417d897d11bf8c61b19eac962b9
李旭 <lixu@lixudemacbook-pro.local>
2016-08-25 20:15:56 +0800

    update

--- LXScrollerCacheChildVC/RootViewController.m
+++ LXScrollerCacheChildVC/RootViewController.m
@@ -79,7 +79,7 @@
 - (void)initSetup
 {
     self.view.backgroundColor = [UIColor grayColor];
-    self.title = @"LXScrollerCacheSubViewCell";
+    self.title = @"横向的tableView";
     [self.navigationController.navigationBar setBarTintColor:[UIColor colorWithRed:0.800 green:0.600 blue:0.800 alpha:0.5]];
     //下面句代码必不可少
     self.automaticallyAdjustsScrollViewInsets = NO;
